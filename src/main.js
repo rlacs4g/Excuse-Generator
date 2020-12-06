78b62125bc0e014dafcea93c53e76ea0cd9120d6
rlacs4g <lacson.rafael@gmail.com>
2020-12-06 20:05:43 +0000

    First Commit

--- src/main.js
+++ src/main.js
@@ -6,5 +6,30 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let sentence =
+    who[getRndInteger(0, 3)] +
+    " " +
+    action[getRndInteger(0, 3)] +
+    " " +
+    what[getRndInteger(0, 2)] +
+    " " +
+    when[getRndInteger(0, 4)];
+
+  function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  var excuse = document.getElementById("excuse");
+  excuse.innerHTML = sentence;
 };
